f6e4f8fb2789a154112ba63467e8865ccca86442
Antrikas <andreas.antoniades17@gmail.com>
2023-11-01 10:42:03 +0200

    JSBC

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('it worked')
+//function addition  (num1, num2) {
+   // return num1 + num2
+//}
+
+//console.log(addition(-3, -6));
+
+
+function hoursIntoSeconds(hour) {
+let minutes = hour * 60;
+let seconds = minutes * 60;
+return seconds;
+}
+
+console.log(hoursIntoSeconds(2));
+
